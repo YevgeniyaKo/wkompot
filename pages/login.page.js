f0c0ac1732bd416209dc3294de5925586a0de447
yevgeniya <yevgeniya.kozhanova@gmail.com>
2021-12-01 16:53:48 -0800

    add client tests

--- pages/login.page.js
+++ pages/login.page.js
@@ -1,21 +1,21 @@
 import Page from './page';
 
 class LoginPage extends Page {
-    get inputUsername () { return $('[qa-id="email"]') }
-    get inputPassword () { return $('[qa-id="password"]') }
-    get buttonSubmit () { return $('.login-form-button') }
+    get inputUsername() { return $('[qa-id="email"]') }
+    get inputPassword() { return $('[qa-id="password"]') }
+    get buttonSubmit() { return $('.login-form-button') }
     get notification() { return $('.ant-notification-notice-message') }
     get loginError() { return $('//div[@data-qa="normalEmail"]//div[@class="ant-form-item-explain-error"]') }
-    get passwordError() { return $('//div[div[@class="ant-form-item-control-input"]//input[qa-id="password"]]//div[@class="ant-form-item-explain-error"]') }
+    get passwordError() { return $('//div[div[@class="ant-form-item-control-input"]//input[@qa-id="password"]]//div[@class="ant-form-item-explain-error"]') }
 
-    async login (username, password) {
+    async login(username, password) {
         await this.inputUsername.setValue(username);
         await this.inputPassword.setValue(password);
-        await expect(this.buttonSubmit).toBeEnabled()
+        await expect(this.buttonSubmit).toBeEnabled();
         await this.buttonSubmit.click();
     }
 
-    open () {
+    open() {
         return super.open('/user/login');
     }
 }
